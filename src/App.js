a1790a1acc0a3127128ec84a9191b00f268b61d1
NECKERRMAN <robin.deneckere@gmail.com>
2022-03-17 21:50:42 +0100

    First commit, created components

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Pages from './pages/Pages';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+    <Pages />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Pages.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Home from './Home'
+
+const Pages = () => {
+  return (
+    <Home />
+  )
+}
+
+export default Pages
